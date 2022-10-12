0f2d304dc89c427d9e1339401888badf044f42df
bhunesh meravi <meravibhunesh0@gmail.com>
2022-10-13 01:17:09 +0530

    make home page

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const ejs = require('ejs')
 const expressLayout = require('express-ejs-layouts')
 const path = require('path')
 
+//Assets
+app.use(express.static('public'))
 app.get('/', (req,res) => {
     res.render('home')
 })
